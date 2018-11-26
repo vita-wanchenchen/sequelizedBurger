0571bcfb31db9576c4599b47406a2f0a1fad0487
Vita Chen <39967407+vita-wanchenchen@users.noreply.github.com>
2018-11-26 00:06:55 -0500

    setup sequelize init

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,16 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+
 
 var burger = {
     all: function(cb) {
-        orm.selectAll("burgers", function(res) {
-            cb(res);
-        });
+
     },
 
     create: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res);
-        });
+
     },
 
     update: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", {devoured: true}, condition, function(res) {
-            cb(res);
-        });
+
     }
 
 };
